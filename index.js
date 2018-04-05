8ce5de91bab64ad999fc8b1bdf3e9981a508a4b9
marcantoineandreoli@gmail.com Andreoli <marcantoineandreoli@gmail.com>
2018-04-05 03:59:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function iterativeLog(array) {
   array.forEach( element => {
-    console.log(`${array.indexOf(element)}: ${element}`));
+    console.log(`${array.indexOf(element)}: ${element}`);
   }
-  //array.forEach(console.log(`${index}: ${element}`));
+  )
+};
+
+function iterativeLog(callBack) {
+  var array = [1,2,3,4,5,6];
+  array.forEach()
+};
+
+function callBack () {
+  return n * 2;
+};
+
+function doToArray(array, callBack) {
+
 };
